bb71b36c7470937d3058f025deb94ef8046ca719
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-01-30 19:36:19 +0200

    final version of initial version

--- azure_infra/paas/asp.tf
+++ azure_infra/paas/asp.tf
@@ -2,6 +2,8 @@ resource "azurerm_app_service_plan" "asp" {
     name = "${var.prefix}-asp"
     resource_group_name = "${azurerm_resource_group.rg.name}"
     location = "${var.location}"
+    kind = "Linux"
+    reserved = true
     sku {
         capacity = 1
         size = "S1"
--- azure_infra/paas/webapp.tf
+++ azure_infra/paas/webapp.tf
@@ -1,6 +1,16 @@
 resource "azurerm_app_service" "webapp" {
     name = "${var.prefix}-app"
-    location = "${var.prefix}"
+    location = "${var.location}"
     resource_group_name = "${azurerm_resource_group.rg.name}"
     app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
+    site_config {
+        always_on = true
+    }
 }
+
+# resource "azurerm_app_service_source_control" "webapp-github" {
+#     app_service_id = "${azurerm_app_service.webapp.id}"
+#     repo_url = "https://github.com/Sebastian-Negoescu/myprofile.git"
+#     branch = "master"
+#     is_manual_integration = true
+# }
