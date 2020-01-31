633898981c89e1d828fb0212dcb845c0a458cb45
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-01-31 16:48:01 +0200

    adding slots: develop && feature/az for deployments.

--- azure_infra/paas/webapp.tf
+++ azure_infra/paas/webapp.tf
@@ -8,6 +8,27 @@ resource "azurerm_app_service" "webapp" {
     }
 }
 
+resource "azurerm_app_service_slot" "webapp-develop" {
+    name = "develop"
+    location = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    app_service_name = "${azurerm_app_service.webapp.name}"
+    app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
+    site_config {
+        always_on = true
+    }
+}
+
+resource "azurerm_app_service_slot" "webapp-feature" {
+    name = "feature"
+    location = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    app_service_name = "${azurerm_app_service.webapp.name}"
+    app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
+    site_config {
+        always_on = true
+    }
+}
 # resource "azurerm_app_service_source_control" "webapp-github" {
 #     app_service_id = "${azurerm_app_service.webapp.id}"
 #     repo_url = "https://github.com/Sebastian-Negoescu/myprofile.git"
