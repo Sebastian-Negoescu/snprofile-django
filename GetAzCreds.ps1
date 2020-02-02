49569b07aa8ebcc8833686319265d3675b7ba04f
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-02-02 13:23:53 +0200

    get az creds for dev script

---
+++ GetAzCreds.ps1
@@ -0,0 +1,28 @@
+$oldWs = Get-Location
+$secret_file = $oldWs.ToString() + "\azure_infra\paas\secrets.tfvars"
+$secret_content = Get-Content -Path $secret_file
+
+[string]$subId;
+[string]$tenant_id;
+[string]$spId;
+[string]$spPwd;
+ForEach ($line in $secret_content) {
+    $lineSplit = $line.Split(' ')
+    If ($lineSplit[0].ToString() -eq "subscription_id") {
+        $subId = $lineSplit[2]
+    }
+    elseif ($lineSplit[0].ToString() -eq "tenant_id") {
+        $tenant_id = $lineSplit[2]
+    }
+    elseif ($lineSplit[0].ToString() -eq "client_id") {
+        $spId = $lineSplit[2]
+    }
+    Else { #($lineSplit[0].ToString() -eq "client_secret")
+        $spPwd = $lineSplit[2]
+    }
+}
+
+Write-Host "Subscription ID: $($subId)" -ForegroundColor Black -BackgroundColor Yellow
+Write-Host "Tenant ID: $($tenant_id)" -ForegroundColor Black -BackgroundColor Yellow
+Write-Host "ServicePrincipal ID: $($spId)" -ForegroundColor Black -BackgroundColor Yellow
+Write-Host "ServicePrincipal Secret: Is somone actually going to believe I was about to show my SP's secret...? :(" -ForegroundColor Black -BackgroundColor Yellow
