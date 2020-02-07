6acb5dc22f427e362e51805141629a1dfb5a51a2
SebiNego@MacBook <sebastian.negoescu.c@thalesdigital.io>
2020-02-07 15:43:43 +0200

    continue decoration && func for time_dif

--- myjobs/models.py
+++ myjobs/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils.timesince import timesince
 
 # Create your models here.
 class Job(models.Model):
@@ -10,9 +12,22 @@ class Job(models.Model):
     end_date = models.DateField(blank=True, null=True)
     projects = models.TextField(max_length=5000, blank=True, null=True)
     photo = models.ImageField(upload_to="media/", null=True, blank=False, default="/media/unavailable.jpg")
-    
+
     def __str__(self):
         return f"{self.title} at {self.company}"
+
+    def get_time_dif(self):
+        dummytime = datetime.time(0, 0)
+        dummydate = datetime.date.today()
+        if (self.end_date):
+            dt1 = datetime.datetime.combine(self.start_date, dummytime)
+            dt2 = datetime.datetime.combine(self.end_date, dummytime)
+            return timesince(dt1, dt2)
+        else:
+            dt1 = datetime.datetime.combine(self.start_date, dummytime)
+            dt2 = datetime.datetime.combine(dummydate, dummytime)
+            return timesince(dt1, dt2)
+
 
 class Technology(models.Model):
     name = models.CharField(max_length=30)
